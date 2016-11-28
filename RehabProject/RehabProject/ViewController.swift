a7f71e054465ceaee276f8f63aa6c8f3750f53bc
toshiecherry <elin_sakurai@hotmail.com>
2016-11-28 11:20:33 +0100

    Initial creating

---
+++ RehabProject/RehabProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RehabProject
+//
+//  Created by Elin Sakurai on 2016-11-28.
+//  Copyright © 2016 Elin Sakurai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
